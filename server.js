c9c26fd1da0d86e24528da60c2f7b08aa4917e32
danazou <dana.sou@gmail.com>
2022-11-13 11:34:19 -0800

    delete route complete

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
+
 
 // CONFIGURATION
 require('dotenv').config()
@@ -10,6 +12,8 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(methodOverride('_method'))
+
 
 // ROUTES
 app.get('/', (req, res) => {
