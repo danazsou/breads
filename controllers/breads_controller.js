b6b3bc57fae378a255b81bc575f011598c40c05a
danazou <dana.sou@gmail.com>
2022-11-15 14:01:05 -0800

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -15,7 +15,6 @@ breads.get('/', (req, res) => {
 })
 
 // CREATE
-// CREATE
 breads.post('/', (req, res) => {
   if (!req.body.image) {
     req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
@@ -29,7 +28,10 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
-
+// NEW
+breads.get('/new', (req, res) => {
+  res.render('new')
+})
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
@@ -49,10 +51,28 @@ breads.delete('/:indexArray', (req, res) => {
   res.status(303).redirect('/breads')
 })
 
-// NEW
-breads.get('/new', (req, res) => {
-  res.render('new')
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
+
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+
+
+
 
 
 
--- views/show.jsx
+++ views/show.jsx
@@ -22,6 +22,9 @@ function Show ({bread, index}) {
   
 <form action={`/breads/${index}?_method=DELETE`} method="POST">
   <input type='submit' value="DELETE"/>
+  
+  <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
 </form>
 
 </Default>
