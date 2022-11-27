d35cfae9aaf8ac478ce2d856b4084fbe5c14e52b
danazou <dana.sou@gmail.com>
2022-11-26 20:29:04 -0800

    added virtuals but not connecting to db properly. Need to debug

--- models/baker.js
+++ models/baker.js
@@ -1,23 +1,32 @@
 // dependencies
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const Bread = require('./bread')
 
-// schema
-const bakerSchema = new Schema({
-    name: {
+// SCHEMA
+const bakerSchema = new Schema(
+    {
+      name: {
         type: String,
         required: true,
-        enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
-    }, 
-    startDate: {
+        enum: ["Rachel", "Monica", "Joey", "Chandler", "Ross", "Phoebe"],
+      },
+      startDate: {
         type: Date,
-        required: true
+        required: true,
+      },
+      bio: String,
     },
-    bio: String
-})
-
-
-
-// model and export
-const Baker = mongoose.model('Baker', bakerSchema)
-module.exports = Baker
+    { toJSON: { virtuals: true } }
+  );
+  
+  // VIRTUALS:
+  bakerSchema.virtual("breads", {
+    ref: "Bread",
+    localField: "_id",
+    foreignField: "baker",
+  });
+  
+  // MODEL & EXPORT
+  const Baker = mongoose.model("Baker", bakerSchema);
+  module.exports = Baker;
--- views/index.jsx
+++ views/index.jsx
@@ -1,34 +1,39 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Index ({breads, title})  {
-    return (
-        <Default>
-  <h2>{title}</h2>
-  {/* <p>I have {breads[0].name} bread!</p> */}
-  {/* This is a JSX comment. */}
-  <ul>
-  {breads.map((bread, index)=> {
-    return (
-      <li key={index}>
-       <a href={`/breads/${bread.id}`}>
-    {bread.name}
-</a>
-
-      </li>
-    )
-  })
+function Index({ breads, bakers, title }) {
+  return (
+    <Default title={title}>
+      <h2>Index Page</h2>
+      {/* <p>I have {breads[0].name} bread!</p> */}
+      {/* This is a JSX comment. */}
+      <h3>Bakers</h3>
+      <ul>
+        {bakers.map((baker) => {
+          return (
+            <li key={baker.id}>
+              <a href={`/bakers/${baker.id}`}>{baker.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+      <h3>Breads</h3>
+      <div className="newButton">
+        <a href="/breads/new">
+          <button>Add a new bread</button>
+        </a>
+      </div>
+      <ul>
+        {breads.map((bread, index) => {
+          return (
+            <li key={index}>
+              <a href={`/breads/${bread.id}`}>{bread.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </Default>
+  );
 }
 
-  </ul>
-
-  <div className="newButton">
-      <a href="/breads/new"><button>Add a new bread</button></a>
-  </div>
-</Default>
-
-      
-    )
-}
-
-module.exports = Index
+module.exports = Index;
